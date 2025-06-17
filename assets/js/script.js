ea7e8b0e4b5e8d1ba664b595da6c5a90ea3bce20
JordanTrehin76610 <jordantrehinpro@gmail.com>
2025-06-17 16:24:20 +0200

    Initialisation

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+// function majuscule() {
+
+//     let message = prompt("Ecrivez un mot")
+
+//     phrase = message.toLowerCase().split('')
+//     phrase[0] = phrase[0].toUpperCase()
+//     phrase = phrase.join('')
+//     return phrase
+
+// }
+
+// alert(majuscule())
+
+
+
+
+
+function bienvenue(nom) {
+
+    return alert(`Bonjour ${nom}, bienvenue à la formation DWWM`)
+
+}
+
+bienvenue(prompt("Inscrivez votre nom"))
